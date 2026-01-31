397eafff72a85bae808a42c1f94a9164c917020b
M Ridhwan K <mrdhwnkml@gmail.com>
2026-01-31 11:34:37 +0700

    dev

--- register.cy.js
+++ register.cy.js
@@ -1,5 +1,5 @@
 describe('register Test Suite', () => {
-    it('should successfully register with valid credentials', () => {
+    it('should successfully register with valid credentials dev', () => {
         cy.visit('https://example.com/register');
         cy.get('#username').type('newUser');
         cy.get('#email').type('newUser@example.com');
